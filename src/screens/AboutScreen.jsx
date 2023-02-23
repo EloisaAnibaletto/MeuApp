6d579d969fba3234c95819b1e86ae422d746b1e0
Eloisa Anibaletto <eloisa.anibaletto2@gmail.com>
2023-02-22 21:58:37 -0300

    alteracoes

--- src/screens/AboutScreen.jsx
+++ src/screens/AboutScreen.jsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Button } from "react-native-web";
 
-export default function AboutScreen(){
-    return(
-        <View>
-            <Text> Sou a página </Text>
-        </View>
-    )
+export default function AboutScreen() {
+  return (
+    <View>
+      <Text> Sou a página </Text>
+      <Button></Button>
+    </View>
+  );
 }
--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function ContactScreen(){
     return(
         <View>
             <Text> Oi sou a página de contato</Text>
+            <Button 
+            mode="contained"
+            onPress={()=>navigation.navigate("Home")}
+            >Ir para Home</Button>
         </View>
     )
 }
