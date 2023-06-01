107c1fec4c4dc69b7d015a35bb0945e4e62eaf7d
Your Name <you@example.com>
2023-05-31 21:59:33 -0300

    Fixes

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { Provider as PaperProvider } from "react-native-paper";
 import RootNavigation from "./src";
+import { useColorScheme } from "react-native";
+import themes from "./src/utils/theme";
 
 export default function App() {
+  const prefersDarkMode = useColorScheme() === "dark";
+
+  const themeDark = themes.dark;
+  const themeLight = themes.light;
+
+  const theme = prefersDarkMode ? themes.dark : themes.light;
+
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <RootNavigation />
     </PaperProvider>
   );
---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import { Image } from "react-native";
+
+export default function Logo() {
+  return (
+    <Image
+      source={require("/assets/inicio1.png")}
+      style={{
+        width: 250,
+        height: 250,
+      }}
+      resizeMode="contain"
+    />
+  );
+}
